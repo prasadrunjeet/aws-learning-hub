24d58cddc4884f3b909a280e982c12ca436c3e89
Runjeet <prasadrunjeet@gmail.com>
2026-08-09 11:25:37 +0530

    projetc folder name is updated

--- visitor-counter-backend/backend/app/__init__.py
+++
@@ -1,7 +0,0 @@
-"""app package initializer.
-
-This file makes the `app` directory a Python package.
-In a growing application, it also becomes the place for package-level setup.
-"""
-
-# We keep this file minimal for now.
